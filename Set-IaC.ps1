f85daab2a52425e740f5402c458141b63142f0bd
razidare <razidare@barbart.com>
2023-05-17 10:56:04 +0300

    script

---
+++ Set-IaC.ps1
@@ -0,0 +1,52 @@
+<#
+.Synopsis
+   Script used to run terraform layers in order + to destroy them in order
+.EXAMPLE
+   ./Set-IaC.ps1 -action create
+   ./Set-IaC.ps1 -action destroy
+#>
+
+param (
+    [Parameter(Mandatory=$true)][ValidateSet("create", "destroy")][string]$action
+)
+
+$globalFolder = "$PSScriptRoot/global"
+$envFolder = "$PSScriptRoot/environment"
+
+switch ($action) {
+    "create" {
+        # move to global folder and run tf commands
+        Push-Location $globalFolder
+        Write-Verbose "Running Inside $globalFolder - Command is APPLY" -vb
+        terraform init
+        terraform apply -auto-approve
+
+        Pop-Location
+
+        # move to env folder and run tf commands
+        Push-Location $envFolder
+        Write-Verbose "Running Inside $envFolder - Command is APPLY" -vb
+        terraform init
+        terraform apply -auto-approve
+
+        Pop-Location
+    }
+    "destroy" {
+        # move to env folder and run tf commands
+        Push-Location $envFolder
+        Write-Verbose "Running Inside $envFolder - Command is DESTROY" -vb
+        terraform init
+        terraform destroy -auto-approve
+
+        Pop-Location
+
+        #move to global folder and run tf commands
+        Push-Location $globalFolder
+        Write-Verbose "Running Inside $globalFolder - Command is DESTROY" -vb
+        terraform init
+        terraform destroy -auto-approve
+
+        Pop-Location
+    }
+}
+
